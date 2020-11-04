d2e231ac784c8610f4fdbda146e92662de10f386
SHIVAM-hash <69781099+SHIVAM-hash@users.noreply.github.com>
2020-11-04 19:44:34 +0530

    Update serializers.py

--- ecom/api/category/serializers.py
+++ ecom/api/category/serializers.py
@@ -1,11 +1,7 @@
-from django.db import models
+from rest_framework import serializers
+from .models import Category
 
-# Create your models here.
-
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=250)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    def __str__(self):
-        return self.name
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name','description')
